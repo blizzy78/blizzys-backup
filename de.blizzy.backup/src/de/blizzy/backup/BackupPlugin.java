e93f5fb25eeca7d40f4e166d93049b0ba7985ff7
Maik Schreiber <blizzy@blizzy.de>
2011-10-06 16:32:14 +0000

    version 1.2.6

--- de.blizzy.backup/src/de/blizzy/backup/BackupPlugin.java
+++ de.blizzy.backup/src/de/blizzy/backup/BackupPlugin.java
@@ -42,7 +42,7 @@ import de.blizzy.backup.vfs.filesystem.FileSystemLocationProvider;
 
 public class BackupPlugin extends AbstractUIPlugin {
 	public static final String ID = "de.blizzy.backup"; //$NON-NLS-1$
-	public static final String VERSION = "1.2.5"; //$NON-NLS-1$
+	public static final String VERSION = "1.2.6"; //$NON-NLS-1$
 	public static final String COPYRIGHT_YEARS = "2011"; //$NON-NLS-1$
 
 	public static final int KEEP_HOURLIES_DAYS = 7;

d05018de8746be79e728e8bb46556540d09514cb
Maik Schreiber <blizzy@blizzy.de>
2012-03-18 15:48:06 +0100

    version 1.5.4; RCP feature version 1.0.2

--- de.blizzy.backup/src/de/blizzy/backup/BackupPlugin.java
+++ de.blizzy.backup/src/de/blizzy/backup/BackupPlugin.java
@@ -42,7 +42,7 @@ import de.blizzy.backup.vfs.filesystem.FileSystemLocationProvider;
 
 public class BackupPlugin extends AbstractUIPlugin {
 	public static final String ID = "de.blizzy.backup"; //$NON-NLS-1$
-	public static final String VERSION = "1.5.3"; //$NON-NLS-1$
+	public static final String VERSION = "1.5.4"; //$NON-NLS-1$
 	public static final String COPYRIGHT_YEARS = "2011-2012"; //$NON-NLS-1$
 
 	public static final int KEEP_HOURLIES_DAYS = 7;
